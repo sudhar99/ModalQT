5523c24facc98d1af9de74d3777c542b42eb965e
Sudharsan <sudharsan@droneinch.com>
2021-08-06 10:44:13 -0700

    ls

--- js/ept.js
+++ js/ept.js
@@ -88,7 +88,7 @@ var Ept = function() {
 
 	// Private	
 	function createProgressModal(heading) {		
-		$j('#focusIdAfterDialogClose').attr('disabled', true);
+		$j('#'+focusIdAfterDialogClose).attr('disabled', true);
 		$j('h1').after('<div class="js-modal '+ currentModalClass+'" data-modal-prefix-class="mycal" data-modal-content-id = "'+currentDialogContentId+'" data-focus-back = "'+ focusIdAfterDialogClose +'" data-modal-title = "'+ heading + '" data-modal-describedby-id="'+modalAriaDescId+'" ></div>');
 		$j('h1').after('<div class="mycal-sr-only" id="'+currentDialogContentId+'"></div>');
 		$j('#'+currentDialogContentId).append('<p>Your request is being processed</p><progress id="modalProgressBar"></progress>');
